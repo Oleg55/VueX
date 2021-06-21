baad5408c33f46fe7ff1242a53d217674e536b90
Oleg55 <Shevchenko.oleg.1992@gmail.com>
2021-06-21 16:48:38 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="Todo">
+        <h1>Your actions</h1>
+        <app-action 
+            v-for="(action,i) in todoList"
+            v-bind:title="action.title"
+            v-bind:current="action.current"
+            v-bind:max="action.max"
+            v-bind:key="i"
+            v-on:step="onNext(i)"
+            ></app-action>
+    </div>
+</template>
+<script>
+import AppAction from '@/components/Action.vue'
+
+export default {
+    components: {
+        AppAction
+    },  
+    data: () => ({
+
+        todoList: [
+            { title: 'First action', current: 1, max: 5 },
+            { title: 'Other work', current: 2, max: 10 },
+            { title: 'Some fun', current: 4, max: 7 }
+        ]
+    }),
+    methods: {
+        onNext(i){
+            this.todoList[i].current++;
+        },
+
+    }
+	
+}
+</script>
