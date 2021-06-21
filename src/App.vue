3eccc849c00b3a669b958069eacbc77df35d8fd7
Oleg55 <Shevchenko.oleg.1992@gmail.com>
2021-06-21 18:46:22 +0300

    vueX lesson 11

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,31 @@
 <template>
-    <div class="Todo">
-        <h1>Your actions</h1>
-        <app-action 
-            v-for="(action,i) in todoList"
-            v-bind:title="action.title"
-            v-bind:current="action.current"
-            v-bind:max="action.max"
-            v-bind:key="i"
-            v-on:step="onNext(i)"
-            ></app-action>
-    </div>
+<div>
+	<app-header></app-header>
+	<app-content></app-content>
+</div>
 </template>
+
 <script>
-import AppAction from '@/components/Action.vue'
+	import AppContent from './components/Content';
+	import AppHeader from './components/Header';
 
-export default {
-    components: {
-        AppAction
-    },  
-    data: () => ({
-
-        todoList: [
-            { title: 'First action', current: 1, max: 5 },
-            { title: 'Other work', current: 2, max: 10 },
-            { title: 'Some fun', current: 4, max: 7 }
-        ]
-    }),
-    methods: {
-        onNext(i){
-            this.todoList[i].current++;
-        },
-
-    }
-	
-}
+	export default {
+		components: {
+			AppContent,
+			AppHeader
+		},
+		data(){
+			return {
+				price: 1000,
+				cnt: 1
+			}
+		},
+		methods: {
+			onDescrease(){
+				if(this.cnt > 1){
+					this.cnt--;
+				}
+			}
+		}
+	}
 </script>
